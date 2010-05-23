225eb57bda43cc2067089a94ce48ad26f120f376
Marco Vermeulen <vermeulen.mp@gmail.com>
2010-05-23 21:23:34 +0100

    First working implementation of New Releases.

--- grails-app/services/uk/co/hashcode/itunes/ITunesService.groovy
+++ grails-app/services/uk/co/hashcode/itunes/ITunesService.groovy
@@ -1,12 +1,34 @@
 package uk.co.hashcode.itunes
 
+import com.sun.syndication.fetcher.*
+import com.sun.syndication.fetcher.impl.*
+import com.sun.syndication.feed.synd.SyndFeed
+
+class Album {
+    def name
+}
+
 class ITunesService {
 
     static transactional = false
     
     def url
 
-    def getNewAlbumReleases(criteria) {
-    	return url
+    def getNewAlbumReleases() {
+        FeedFetcherCache feedInfoCache = HashMapFeedInfoCache.getInstance();
+        FeedFetcher feedFetcher = new HttpURLFeedFetcher(feedInfoCache);
+        SyndFeed feed = feedFetcher.retrieveFeed(new URL(url));
+        def albums = []
+        feed.entries.each { album ->
+            albums << new Album(
+                name: album.title
+            )
+//            println " -> ${it.link} "
+//            println " -> ${it.title} "
+//            println " -> ${it.taxonomyUri}"
+
+        }
+
+    	return albums
     }
 }
--- test/integration/uk/co/hashcode/itunes/ITunesServiceIntegrationTests.groovy
+++ test/integration/uk/co/hashcode/itunes/ITunesServiceIntegrationTests.groovy
@@ -4,19 +4,31 @@ import grails.test.*
 
 class ITunesServiceIntegrationTests extends GrailsUnitTestCase {
     def iTunesService
-    def url    
+    def url
+    def parameters
+        
     protected void setUp() {
         super.setUp()
-        url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topalbums/sf=143441/limit=1/genre=14/xml'
-        iTunesService = new ITunesService(url:url)
+        url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa'
+        parameters = 'ws/RSS/topalbums/sf=143441/limit=10/genre=14/xml'
+        iTunesService = new ITunesService(url:"${url}/${parameters}")
+    }
+
+    void testGetNewAlbumReleases() {
+        def releases = iTunesService.getNewAlbumReleases()
+        
+        assert releases != null
+        assert releases.size() == 10
+        
+        releases.each { album ->
+            assert album
+            assert album.name
+            println album.name
+        }
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testGetNewAlbumReleases() {
-        def releases = iTunesService.getNewAlbumReleases(1)
-        assert releases == url
-    }
 }
