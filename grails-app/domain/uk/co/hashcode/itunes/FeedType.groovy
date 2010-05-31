393381a58c885b80f43202669c1ea50ae13dfe60
Marco Vermeulen <vermeulen.mp@gmail.com>
2010-05-31 15:55:31 +0100

    Added context and subContext to FeedType

--- grails-app/domain/uk/co/hashcode/itunes/FeedType.groovy
+++ grails-app/domain/uk/co/hashcode/itunes/FeedType.groovy
@@ -1,18 +1,22 @@
 package uk.co.hashcode.itunes
 
 enum FeedType {
-	NEW_RELEASES('MZStore.woa', 'newreleases'),
-	JUST_ADDED('MZStore.woa', 'justadded'),
-	FEATURED_ALBUMS('MZStore.woa', 'featuredalbums'),
-	TOP_ALBUMS('MZStoreServices.woa', 'topalbums'),
-	TOP_IMIXES('MZStoreServices.woa', 'topimixes'),
-	TOP_SONGS('MZStoreServices.woa', 'topsongs')
+	NEW_RELEASES('MZStore.woa', 'wpa', 'MRSS', 'newreleases'),
+	JUST_ADDED('MZStore.woa', 'wpa', 'MRSS', 'justadded'),
+	FEATURED_ALBUMS('MZStore.woa', 'wpa', 'MRSS', 'featuredalbums'),
+	TOP_ALBUMS('MZStoreServices.woa', 'ws', 'RSS', 'topalbums'),
+	TOP_IMIXES('MZStoreServices.woa', 'ws', 'RSS', 'topimixes'),
+	TOP_SONGS('MZStoreServices.woa', 'ws', 'RSS', 'topsongs')
 	
 	String woa
+	String context
+	String subContext
 	String service
 	
-	public FeedType(String woa, String service){
+	public FeedType(String woa, String context, String subContext, String service){
 		this.woa = woa
+		this.context = context
+		this.subContext = subContext
 		this.service = service
 	}
 	
