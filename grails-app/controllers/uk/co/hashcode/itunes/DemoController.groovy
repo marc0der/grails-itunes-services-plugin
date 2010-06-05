d20b30a5d5472777043057251713c1bc7aa3799d
Marco Vermeulen <vermeulen.mp@gmail.com>
2010-06-05 07:59:38 +0100

    Added FeedsCommand filtering in the DemoController.

--- grails-app/controllers/uk/co/hashcode/itunes/DemoController.groovy
+++ grails-app/controllers/uk/co/hashcode/itunes/DemoController.groovy
@@ -5,11 +5,36 @@ class DemoController {
     def itunesFeedsService
 
     def index = { 
-        def command = new FeedsCommand()
-        def newReleases = itunesFeedsService.getNewAlbumReleases(command)
-        def justAddedAlbums = itunesFeedsService.getJustAddedAlbums(command)
-        def featuredAlbums = itunesFeedsService.getFeaturedAlbums(command)
-        def topAlbums = itunesFeedsService.getTopAlbums(command)
+        //new releases
+    	def newReleaseCommand = new FeedsCommand(
+        		genre:Genre.POP,
+        		limit:10,
+        		country:Country.UK
+        )
+        def newReleases = itunesFeedsService.getNewAlbumReleases(newReleaseCommand)
+        
+        //just added
+        def justAddedCommand = new FeedsCommand(
+        		genre:Genre.ROCK,
+        		limit:5,
+        		country:Country.JAPAN
+        )
+        def justAddedAlbums = itunesFeedsService.getJustAddedAlbums(justAddedCommand)
+        
+        //featured albums
+        def featuedCmd = new FeedsCommand(
+        		genre:Genre.POP,
+        		limit:4,
+        		country:Country.GERMANY
+        )
+        def featuredAlbums = itunesFeedsService.getFeaturedAlbums(featuedCmd)
+        
+        //top albums
+        def topCmd = new FeedsCommand(
+        		genre:Genre.ALL,
+        		limit:10,
+        		country:Country.USA)
+        def topAlbums = itunesFeedsService.getTopAlbums(topCmd)
         
         def results = [
 		    newReleases:newReleases, 
@@ -17,6 +42,7 @@ class DemoController {
 		    featuredAlbums:featuredAlbums,
 		    topAlbums:topAlbums
         ]
+        
         render(view:'show', model:[results:results])
     }
 
