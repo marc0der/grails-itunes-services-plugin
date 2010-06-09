484826a2f8b28b8e40477f27eb4149eb61c12aa4
Marco Vermeulen <vermeulen.mp@gmail.com>
2010-06-09 07:56:13 +0100

    Fixed typos in Genre names.

--- test/unit/uk/co/hashcode/itunes/GenreTests.groovy
+++ test/unit/uk/co/hashcode/itunes/GenreTests.groovy
@@ -23,7 +23,7 @@ class GenreTests {
     	assert Genre.BLUES.id == 2
     	assert Genre.CHILDREN.id == 4
     	assert Genre.CLASSICAL.id == 5
-    	assert Genre.COMMEDY.id == 3
+    	assert Genre.COMEDY.id == 3
     	assert Genre.COUNTRY.id == 6
     	assert Genre.DANCE.id == 17
     	assert Genre.ELECTRONIC.id == 7
@@ -34,7 +34,7 @@ class GenreTests {
     	assert Genre.POP.id == 14
     	assert Genre.RB_SOUL.id == 15
     	assert Genre.REGGAE.id == 24
-    	assert Genre.RELIGEOUS.id == 22
+    	assert Genre.RELIGIOUS.id == 22
     	assert Genre.ROCK.id == 21
     	assert Genre.SOUNDTRACK.id == 16
     	assert Genre.SPOKEN_WORD.id == 50000061
